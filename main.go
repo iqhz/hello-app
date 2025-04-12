8d1e4f82262dc00996ff65298c2f8923e87a5087
Iqbal <aden.uniq@gmail.com>
2025-04-12 14:59:29 +0700

    update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go App on Kubernetes!")
+	fmt.Fprintf(w, "Hello from Go App on Kubernetes Clustes RKE2!")
 }
 
 func main() {
